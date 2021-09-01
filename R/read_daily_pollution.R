a8004351dbdfa1649283678926477d02693cad0e
Chris Wolfe <wolfeclw@icloud.com>
2021-09-01 15:31:16 -0400

    rename cols read_daily

--- R/read_daily_pollution.R
+++ R/read_daily_pollution.R
@@ -30,34 +30,99 @@ read_daily_pollution <- function(path, n_lines = NULL) {
     d_poll <- readr::read_csv(path, n_max = read_n_lines, show_col_types = FALSE)
   }
 
-  d_poll_names <- names(d_poll)
+  d_poll <- rename_with(d_poll, stringr::str_to_lower)
 
-  if (!'subjectID' %in% d_poll_names) {
-  m_names <- menu(names(d_poll), title = paste0('Column `subjectID` is not in the input data frame.',
-                                          ' Please select the column that represents the participant ID.'))
-  id_rename <- names(d_poll)[m_names]
+  poll_names <- names(d_poll)
 
-  dplyr::rename(d_poll, subjectID = all_of(id_rename))
-
-  message(paste0('Column `', id_rename, '` has been renamed to `subjectID`.'))
-  }
-
-  if ('date' %in% d_poll_names) {
-
-    dt_names <- menu(names(d_poll), title = paste0('Column `date` is not in the input data frame.',
-                                                   ' Please select the column that represents the date for each pollution measurement.'))
-  }
-
-  # if (sum(purrr::map_lgl(d_poll, lubridate::is.Date)) == 0) {
+  # if (!'subjectid' %in% poll_names) {
+  #   m_names <- menu(names(d_poll), title = paste0('Column `subjectid` is not in the input data frame.',
+  #                                                 ' Please select the column that represents the participant ID.'))
+  #   id_rename <- names(d_poll)[m_names]
   #
-  #   dt_names <- menu(names(d_poll), title = 'Column ')
+  #   d_poll <- dplyr::rename(d_poll, subjectid = all_of(id_rename))
+  #   poll_names <- names(d_poll)
+  #
+  #   message(paste0('Column `', id_rename, '` has been renamed to `subjectid`.'))
   # }
 
-  # date_guess <- lubridate::guess_formats(d_poll$date, orders = c('ymd', 'mdy', 'dmy'))
-  # date_guess <- unique(stringr::str_replace_all(date_guess, '[[:punct:]O]', ''))
-  #
-  # date_guess
+  if (!'subjectid' %in% poll_names) {
+    m_names <- menu(names(d_poll), title = paste0('Column `subjectid` is not in the input data frame.',
+                                                  ' Please select the column that represents the participant ID.',
+                                                  '\n Select "0" to exit this menu.'))
 
+    if (m_names == 0) {
+      stop('Operation terminated. You may also manually rename the ID column in the input data frame.',
+           call. = FALSE)
+    }
+
+
+
+    id_rename <- names(d_poll)[m_names]
+
+    d_poll <- dplyr::rename(d_poll, subjectid = all_of(id_rename))
+    poll_names <- names(d_poll)
+
+    message(paste0('Column `', id_rename, '` has been renamed to `subjectid`.'))
+  }
+
+  poll_names2 <- poll_names[poll_names != 'subjectid']
+  need_names <- c("dob", "date", "pm25", "no2", "o3")
+
+
+  # if (sum(!poll_names2 %in% need_names) != 0 | sum(!need_names %in% poll_names2) != 0) {
+  #
+  #   pl <- poll_names2 %in% need_names
+  #   nl <- need_names %in% poll_names2
+  #
+  #   no_match <- c(need_names[!nl], poll_names2[!pl])
+  #
+  #   no_match <- no_match[no_match %in% need_names]
+  #
+  #   stop(paste0('The following column(s) are missing from the input data frame and must be included: \n',
+  #               paste0('- ', '`', crayon::red(no_match), '`', collapse = '\n')),
+  #        call. = FALSE)
+  # }
+
+  # if ('date' %in% poll_names2) {
+  #
+  #   dt_names <- menu(names(d_poll), title = paste0('Column `date` is not in the input data frame.',
+  #                                                  ' Please select the column that represents the date for each pollution measurement.'))
+  # }
+
+  if (sum(purrr::map_lgl(d_poll$date, lubridate::is.Date)) == 0) {
+
+
+    date_guess <- lubridate::guess_formats(d_poll$date, orders = c('ymd', 'mdy'))
+    date_guess <- unique(stringr::str_replace_all(date_guess, '[[:punct:]O]', ''))
+
+    if (stringr::str_to_lower(date_guess) == 'ymd') {
+      d_poll$date <- lubridate::ymd(d_poll$date)
+    } else if (stringr::str_to_lower(date_guess) == 'mdy') {
+      d_poll$date <- lubridate::mdy(d_poll$date)
+    } else {
+      stop('Column `date` could not be parsed. Please check the format.')
+    }
+  }
+
+  if (sum(purrr::map_lgl(d_poll$dob, lubridate::is.Date)) == 0) {
+
+
+    dob_guess <- lubridate::guess_formats(d_poll$dob, orders = c('ymd', 'mdy'))
+    dob_guess <- unique(stringr::str_replace_all(dob_guess, '[[:punct:]O]', ''))
+
+    if (stringr::str_to_lower(dob_guess) == 'ymd') {
+      d_poll$date <- lubridate::ymd(d_poll$dob)
+    } else if (stringr::str_to_lower(dob_guess) == 'mdy') {
+      d_poll$date <- lubridate::mdy(d_poll$dob)
+    } else {
+      stop('Column `dob` could not be parsed. Please check the format.')
+    }
+  }
+
+  d_poll
 }
+
+
+
 
 read_daily_pollution(pm_path, n_lines = 10)
